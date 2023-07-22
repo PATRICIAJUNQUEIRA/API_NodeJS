610c1aea0e6f813ee13345d43f06c5209d24ac8a
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-07-22 15:28:07 -0300

    Conexão com o banco de dados MySql

---
+++ backend/src/router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+// router.get('/tasks', (req, res) => res.status(200).send('O Router está funcionando!'));
+
+module.exports = router;
